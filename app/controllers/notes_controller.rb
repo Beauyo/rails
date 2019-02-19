e87a6c1ccac4fcd5ab16efa8a41e7c687509af29
Beau Hicken <Turbodiesel2500dee@hotmail.com>
2019-02-18 20:40:20 -0700

    commit

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -12,17 +12,37 @@ class NotesController < ApplicationController
   end
 
   def create
-    @note = Note.new(Notes_params)
+    @note = Note.new(notes_params)
 
     if @note.save
       redirect_to notes_path
     else
       render :new
     end
-
-    private
-    def Notes_params
-      params.require(:page).permit(:title, :author, :body)
-    end
+  end
     
+ 
+  def edit
+    @note = Note.find(params[:id])
+  end
+
+  def update
+    @note = Note.find(params[:id])
+
+    if @note.update(notes_params)
+      redirect_to notes_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    Note.find(params[:id]).destroy
+   redirect_to notes_path
+  end
+
+  private
+  def notes_params
+    params.require(:note).permit(:title, :author, :body)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root "notes#index"
+
+  get "/about", to: "static_notes#about"
+
 resources :notes
 end
